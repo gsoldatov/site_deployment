71510de3c1b169ea8ba2f917334e32675d6f6f1c
-- <smoke571reg@yandex.ru>
2023-01-22 10:44:46 +0300

    Moved config.py into `util` folder

--- monitoring/db/setup.py
+++ monitoring/db/setup.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     import sys, os
     sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../.."))
 
-from monitoring.config import get_config
+from monitoring.util.config import get_config
 
 
 def connect(config):
