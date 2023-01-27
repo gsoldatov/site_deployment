5d5da97bf570fce10ece67db8aef8a8d09b3fc16
-- <smoke571reg@yandex.ru>
2023-01-27 22:24:52 +0300

    Job runner now shares db connection with job classes

--- monitoring/log_fetching/jobs/base_job.py
+++ monitoring/log_fetching/jobs/base_job.py
@@ -2,10 +2,11 @@ class BaseJob:
     """
     Base class for all jobs run by the log_fetching script.
     """
-    def __init__(self, name, args, config, log):
+    def __init__(self, name, args, config, db_connection, log):
         self.name = name
         self.args = args
         self.config = config
+        self.db_connection = db_connection
         self.log = log
     
     def run(self):
