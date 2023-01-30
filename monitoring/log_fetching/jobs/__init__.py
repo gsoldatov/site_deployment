47cc126f95f334e01131b779c8963ca3a7f38d8a
-- <smoke571reg@yandex.ru>
2023-01-30 21:05:13 +0300

    Added fetch job for Nginx access logs

--- monitoring/log_fetching/jobs/__init__.py
+++ monitoring/log_fetching/jobs/__init__.py
@@ -1,9 +1,12 @@
 from monitoring.log_fetching.jobs.fetch_app_access_logs import FetchAppAccessLogs
 from monitoring.log_fetching.jobs.fetch_app_event_logs import FetchAppEventLogs
 from monitoring.log_fetching.jobs.fetch_database_scheduled_jobs_logs import FetchDatabaseScheduledJobsLogs
+from monitoring.log_fetching.jobs.fetch_nginx_access_logs import FetchNginxAccessLogs
 
 job_list = {
     "app_access_logs": FetchAppAccessLogs,
     "app_event_logs": FetchAppEventLogs,
-    "database_scheduled_jobs_logs": FetchDatabaseScheduledJobsLogs
+    "database_scheduled_jobs_logs": FetchDatabaseScheduledJobsLogs,
+    
+    "nginx_access_logs": FetchNginxAccessLogs
 }
