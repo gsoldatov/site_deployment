1e6800145565a21a3b67a89d8482f8efe9763a34
-- <smoke571reg@yandex.ru>
2023-01-30 13:38:35 +0300

    Added fetch jobg for app event & db scheduled operations jobs

--- monitoring/log_fetching/jobs/__init__.py
+++ monitoring/log_fetching/jobs/__init__.py
@@ -1,7 +1,9 @@
 from monitoring.log_fetching.jobs.fetch_app_access_logs import FetchAppAccessLogs
+from monitoring.log_fetching.jobs.fetch_app_event_logs import FetchAppEventLogs
+from monitoring.log_fetching.jobs.fetch_database_scheduled_jobs_logs import FetchDatabaseScheduledJobsLogs
 
 job_list = {
     "app_access_logs": FetchAppAccessLogs,
-    "app_event_logs": None,
-    "database_scheduled_jobs_logs": None
+    "app_event_logs": FetchAppEventLogs,
+    "database_scheduled_jobs_logs": FetchDatabaseScheduledJobsLogs
 }
